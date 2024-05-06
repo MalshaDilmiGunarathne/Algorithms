fa7fa7682482b95d7c0c90b4fe72b2055955eb5a
MalshaDilmiGunarathne <167998749+MalshaDilmiGunarathne@users.noreply.github.com>
2024-05-06 22:44:41 +0530

    Add files via upload

---
+++ BubbleSortDesc.java
@@ -0,0 +1,24 @@
+public class BubbleSortDesc {
+    public static void main(String args[]) {
+        int arr[] = {3, 2, 5, 1, 7};
+        int n = arr.length;
+        
+        // Bubble sort algorithm for descending order
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] < arr[j+1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+        // Print sorted array
+        System.out.println("Sorted array in descending order:");
+        for (int i=0; i<arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
