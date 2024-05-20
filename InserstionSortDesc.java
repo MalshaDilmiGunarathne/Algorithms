35f42510093123bca91a8362a7345ad48c3dec77
MalshaDilmiGunarathne <167998749+MalshaDilmiGunarathne@users.noreply.github.com>
2024-05-20 19:43:42 +0530

    Add files via upload

---
+++ InserstionSortDesc.java
@@ -0,0 +1,29 @@
+public class InsertionSortDesc{
+
+	
+	    public static void main(String[] args) {
+	        int[] arr = {3, 2, 5, 1, 7};
+	        insertionSort(arr);
+	        System.out.println("Sorted array in descending order:");
+	        for (int num : arr) {
+	            System.out.print(num + " ");
+	        }
+	    }
+
+	    public static void insertionSort(int[] arr) {
+	        int n = arr.length;
+	        for (int i = 1; i < n; ++i) {
+	            int key = arr[i];
+	            int j = i - 1;
+
+	            /* Move elements of arr[0..i-1], that are
+	               less than key, to one position ahead
+	               of their current position */
+	            while (j >= 0 && arr[j] < key) {
+	                arr[j + 1] = arr[j];
+	                j = j - 1;
+	            }
+	            arr[j + 1] = key;
+	        }
+	    }
+	}
